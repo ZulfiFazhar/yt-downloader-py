bb3e2d446e0abda2b1aa4c30b34adb84729b812f
Zulfi Fadilah Azhar <zulfi.fazhar12@gmail.com>
2023-02-20 11:02:24 +0700

    splify code, add audio downloader and convert to mp3

--- script.py
+++ script.py
@@ -1,92 +1,66 @@
-import pytube
 from pytube.cli import on_progress
+from pytube import YouTube
 import os
 
-option = -1
+menu = -1
 
-def resolusi():
-    global link
-    global reso
-    link = input("Enter Your link here : ")
-    print("Select Resolution : 1. 144p")
-    print("                    2. 240p")
-    print("                    3. 360p")
-    print("                    4. 480p")
-    print("                    5. 720p")
-    print("                    6. Highest")
-    reso = int(input("Your Choise : "))
-
-def firstReso():
-    yt = pytube.YouTube(link,on_progress_callback=on_progress)
-    yt.streams.filter(res="144p").first().download()
-    
-    print(f"Downloaded {link} 144p")
-
-def secondReso():
-    yt = pytube.YouTube(link)
-    yt.streams.filter(res="240p").first().download()
-    print(f"Downloaded {link} 240p")
-
-def thirdReso():
-    yt = pytube.YouTube(link)
-    yt.streams.filter(res="360p").first().download()
-    print(f"Downloaded {link} 360p")
-
-def fourthReso():
-    yt = pytube.YouTube(link)
-    yt.streams.filter(res="480p").first().download()
-    print(f"Downloaded {link} 480p")
-
-def fifthReso():
-    yt = pytube.YouTube(link)
-    yt.streams.filter(res="720p").first().download()
-    print(f"Downloaded {link} 720p")
-    
-def highestReso():
-    yt = pytube.YouTube(link)
-    yt.streams.filter(res="1080p").first().download()
-    print(f"Downloaded {link} highest")
-
-
-def unduh():
-    yt = pytube.YouTube(link)
-    yt.streams.get_lowest_resolution().download()
-    print("Downloaded", link)
-
-def menuEng():
-    global option
-    print("           Menu")
-    print("          ------")
+def menuAll():
+    global menu
+    print("Menu")
+    print("----")
     print()
-    print("1. Download YouTube Video")
-    print("2. Download Music from YouTube (Audio Only)")
-    print("0. Exit")
-    option = int(input("Choose Your Option : "))
+    print("1. Download Video from Youtube")
+    print("2. Download Audio from Youtube")
+    print("0. Exit Menu")
+    menu = int(input("Choose Your Option : "))
 
+def downloadVideo():
+    global videos
+    global n
+    videos = yt.streams.filter(type='video', progressive='True')
+    s = 1
+    for vid in videos:
+        print(str(s)+". "+str(vid))
+        s += 1
+    n = int(input("Your Choise : "))
 
-while (option != 0):
+def downloadAudio():
+    global audios
+    global n
+    audios = yt.streams.filter(only_audio="True")
+    s = 1
+    for audio in audios:
+        print(str(s)+". "+str(audio))
+        s += 1
+    n = int(input("Your Choise : "))
+
+def renameAudio():
+    originalFile = audio.default_filename
+    newFile = originalFile.replace(".webm", ".mp3")
+    newFile = originalFile.replace(".mp4", ".mp3")
+    os.rename(originalFile, newFile)
+
+while(menu != 0):
     os.system('cls')
-    menuEng()
-    if (option == 1):
-        print()
-        resolusi()
-        if (reso == 1):
-            firstReso()
-            os.system("pause")
-        elif (reso == 2):
-            secondReso()
-            os.system("pause")
-        elif (reso == 3):
-            thirdReso()
-            os.system("pause")
-        elif (reso == 4):
-            fourthReso()
-            os.system("pause")
-        elif (reso == 5):
-            fifthReso()
-            os.system("pause")
-        elif (reso == 6):
-            highestReso()
-            os.system("pause")
-        else:
-            print("Must be 1-6")
+    menuAll()
+    if(menu == 1):
+        os.system('cls')
+        print("Download Video from Youtube")
+        link = str(input("Enter your link here : "))
+        yt = YouTube(link, on_progress_callback=on_progress)
+        downloadVideo()
+        vid = videos[n-1]
+        vid.download()
+        print(f"{yt.title} \nDownload Completed")
+        os.system('pause')
+    elif(menu == 2):
+        os.system('cls')
+        print("Download Audio from Youtube")
+        link = str(input("Enter your link here : "))
+        yt = YouTube(link, on_progress_callback=on_progress)
+        downloadAudio()
+        audio = audios[n-1]
+        audio.download()
+        renameAudio()
+        print(f"{yt.title} \nDownload Completed")
+        os.system('pause')
